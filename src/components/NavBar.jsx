6416840cac55fe4e3bbc4848f6eff53c781ad4c5
Tiavina3827 <hei.tiavina.4@gmail.com>
2024-09-06 17:16:26 +0300

    style: replace the logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 function NavBar(){
     return(
       <nav>
-          <Link to="/" className="navLinks"><img src="src/assets/logo.png" alt="logo" id="logo"/></Link>
+          <Link to="/" className="logo">Money.</Link>
           <Link to="/" className="navLinks">Home</Link>
           <Link to="/patrimoine" className="navLinks">Patrimoine' stats</Link>
           <Link to="/Possession" className="navLinks">Possession's list</Link>
